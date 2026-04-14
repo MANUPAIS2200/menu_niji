0c94974614c2e1a538be54c2ae6bc3c85a3e8713
manuel pais <manupais22@gmail.com>
2026-04-14 13:39:44 -0300

    cambio de confi

--- firebase-config.js
+++ firebase-config.js
@@ -2,15 +2,15 @@
 // La encontrás en:
 // Configuración del proyecto > General > Tus apps > SDK setup and configuration
 
-export const firebaseConfig = {
-  apiKey: "PEGAR_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_PROYECTO.firebasestorage.app",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyC4v0tuI0qCHvyjDRZm3cHPTvwgtkes7xE",
+  authDomain: "niji-app-2ac82.firebaseapp.com",
+  projectId: "niji-app-2ac82",
+  storageBucket: "niji-app-2ac82.firebasestorage.app",
+  messagingSenderId: "171293465097",
+  appId: "1:171293465097:web:e2a9775e6be3a72b8836d8",
+  measurementId: "G-0J6Y4Z2EFD"
 };
-
 // Si tu colección tiene otro nombre, cambiá esto.
 export const MENU_ITEMS_COLLECTION = "menuItems";
 
