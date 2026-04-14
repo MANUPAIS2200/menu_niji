75bc02c8784905e156329e3844245cfede008c32
manuel pais <manupais22@gmail.com>
2026-04-14 13:48:19 -0300

    sss

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,4 @@
-// Pegá acá TU configuración web de Firebase.
-// La encontrás en:
-// Configuración del proyecto > General > Tus apps > SDK setup and configuration
-
-
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyC4v0tuI0qCHvyjDRZm3cHPTvwgtkes7xE",
   authDomain: "niji-app-2ac82.firebaseapp.com",
   projectId: "niji-app-2ac82",
@@ -12,8 +7,6 @@ const firebaseConfig = {
   appId: "1:171293465097:web:e2a9775e6be3a72b8836d8",
   measurementId: "G-0J6Y4Z2EFD"
 };
-// Si tu colección tiene otro nombre, cambiá esto.
-export const MENU_ITEMS_COLLECTION = "menuItems";
 
-// Si querés ordenar por otro campo, podés cambiarlo.
+export const MENU_ITEMS_COLLECTION = "menuItems";
 export const DEFAULT_ORDER_FIELD = "order";
